42686dd1ed9efa75903488372bc2aa4f5528fae6
malngaawy <mohager477@gmail.com>
2020-04-23 23:06:11 +0200

    add simple component to test tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 function App() {
-  return <div>Hello Man</div>;
+  return (
+    <div className="flex flex-wrap justify-center items-center w-1/2 h-screen bg-gray-500">
+      <div className="bg-red-600 w-10 h-10 text-4xl text-white cursor-pointer flex justify-center items-center rounded-full hover:shadow-xl">
+        +
+      </div>
+    </div>
+  );
 }
 export default App;
